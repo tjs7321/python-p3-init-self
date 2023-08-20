fca39de9abf1d44cb3d899c35421080463cf4124
Theodore Smith <tjs7321@gmail.com>
2023-08-20 03:47:28 -0400

    done

--- lib/dog.py
+++ lib/dog.py
@@ -1,4 +1,17 @@
 #!/usr/bin/env python3
 
 class Dog:
-    pass
+    def __init__(self, name, breed='Mutt', favorite_toy='Any'):
+        self.name = name
+        self.breed = breed
+        self.favorite_toy = favorite_toy
+
+    def bark(self):
+        print("Woof!")
+
+    def get_adopted(self, owner_name):
+        self.owner = owner_name
+
+fido = Dog("Fido")
+
+snoopy = Dog("Snoopy", "Tennis Ball")
